1b742b9e1c33ee8b419c70b2e8ae9e4096f16a47
Noah Lett <noahblett96@gmail.com>
2022-12-02 18:28:49 +0000

    User can now add a new note

--- cli-project/cli-app.js
+++ cli-project/cli-app.js
@@ -1,11 +1,21 @@
 const fs = require('fs');
 const data = require('./data.json');
 const notes = data.notes;
+const input = process.argv[3];
 
 if (process.argv[2] === 'read') {
   fs.readFile('data.json', 'utf8', data => {
     for (const property in notes) {
       console.log(`${property}` + ': ' + notes[property]);
+    }
+  });
+} else if (process.argv[2] === 'add') {
+  data.notes[data.nextId] = input;
+  data.nextId++;
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), 'utf8', err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
     }
   });
 }
